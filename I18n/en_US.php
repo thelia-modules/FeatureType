034b89f09564d189f91f0ec1991dd583dfa499ca
gilles <gilles.bourgeat@gmail.com>
2015-05-28 20:29:33 +0200

    fix I18n

--- I18n/en_US.php
+++ I18n/en_US.php
@@ -1,11 +1,4 @@
 <?php
+
 return array(
-    "Copy action" => "Copy",
-    "Edit action" => "Edit",
-    "Delete action" => "Delete",
-    "Create action" => "Create",
-    "Close action" => "Close",
-    "Update action" => "Update",
-    "Dissociate action" => "Dissociate",
-    "Associate action" => "Associate",
 );
